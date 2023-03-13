ed7e46b05c12173490514dcaa5bf94537164abeb
Arindam Saha <asaha15071998@gmail.com>
2023-03-13 22:42:19 +0530

    Calculating The Fare And The MInimum Fare

--- src/test/java/org/invoice/InvoiceServiceTest.java
+++ src/test/java/org/invoice/InvoiceServiceTest.java
@@ -13,8 +13,17 @@ public class InvoiceServiceTest {
         double distance =2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance,time);
-        Assertions.assertEquals(25,fare);
+        Assertions.assertEquals(25,fare,0.0);
+    }
 
+    //For Minimum Fare
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinimumFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance,time);
+        Assertions.assertEquals(5,fare,0.0); //delta is for remove extra point values
     }
 
 
