0a238c060b7e94cc0c20cd4eee587dbfd5e6a0ec
Arindam Saha <asaha15071998@gmail.com>
2023-03-14 00:45:21 +0530

    Enhanched Invoice Generator

--- src/test/java/org/invoice/InvoiceServiceTest.java
+++ src/test/java/org/invoice/InvoiceServiceTest.java
@@ -43,10 +43,13 @@ public class InvoiceServiceTest {
         Ride[] rides = { new Ride(2.0, 5),
                          new Ride(0.1, 1)};
 
-        double fare = invoiceGenerator.calculateFare(rides);
-        Assertions.assertEquals(30,fare,0.0);
+        InvoiceSummary summary  = invoiceGenerator.calculateFare(rides);
+        InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+        Assertions.assertEquals(expectedInvoiceSummary, summary);
 
     }
+
+
 
 
 }
