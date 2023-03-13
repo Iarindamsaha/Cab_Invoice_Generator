ce2822038312cc285568012786ad6f38c607163d
Arindam Saha <asaha15071998@gmail.com>
2023-03-13 23:04:40 +0530

    Added Multiple Ride Fare System

---
+++ src/main/java/org/invoice/Ride.java
@@ -0,0 +1,13 @@
+package org.invoice;
+
+public class Ride {
+    public final double distance;
+    public final int time;
+
+    public Ride (double distance, int time) {
+        
+        this.distance = distance;
+        this.time = time;
+
+    }
+}
--- src/test/java/org/invoice/InvoiceServiceTest.java
+++ src/test/java/org/invoice/InvoiceServiceTest.java
@@ -1,29 +1,51 @@
 package org.invoice;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class InvoiceServiceTest {
+
+    InvoiceGenerator invoiceGenerator = null;
+    @BeforeEach
+    void setUp() throws Exception {
+        invoiceGenerator = new InvoiceGenerator();
+    }
 
     //Step - 1
 
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
         double distance =2.0;
         int time = 5;
         double fare = invoiceGenerator.calculateFare(distance,time);
         Assertions.assertEquals(25,fare,0.0);
+
     }
 
     //For Minimum Fare
     @Test
     public void givenLessDistanceOrTime_ShouldReturnMinimumFare(){
-        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+
         double distance = 0.1;
         int time = 1;
         double fare = invoiceGenerator.calculateFare(distance,time);
         Assertions.assertEquals(5,fare,0.0); //delta is for remove extra point values
+
+    }
+
+    //Step - 2
+
+    @Test
+    public void givenMultipleRide_ShouldReturnInvoiceSummary(){
+
+        Ride[] rides = { new Ride(2.0, 5),
+                         new Ride(0.1, 1)};
+
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assertions.assertEquals(30,fare,0.0);
+
     }
 
 
